637fb976c2a2babc7bac34dc47938d91dd5b8150
Guilherme Araujo <99432207+GuilhermeAraujo062@users.noreply.github.com>
2022-11-23 20:23:39 -0300

    att

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
-var watch = "";
-
 const start = document.querySelector("#start");
+const pause = document.querySelector("#pause");
+const reset = document.querySelector("#reset");
 
+var hh = 0;
+var mm = 0;
+var ss = 0;
+
+var cron;
