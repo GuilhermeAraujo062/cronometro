b2387a07592bdd4e4cf42b67a2430ec89c5419c5
Guilherme Araujo <99432207+GuilhermeAraujo062@users.noreply.github.com>
2022-11-23 22:26:47 -0300

    finalizado

--- js/script.js
+++ js/script.js
@@ -1,9 +1,73 @@
-const start = document.querySelector("#start");
-const pause = document.querySelector("#pause");
-const reset = document.querySelector("#reset");
+const start = document.querySelector("#istart");
+const pause = document.querySelector("#ipause");
+const reset = document.querySelector("#ireset");
 
-var hh = 0;
-var mm = 0;
-var ss = 0;
+var hor = 0;
+var min = 0;
+var seg = 0;
 
-var cron;
+var coun;
+
+function twoDigits(digit){
+    if(digit < 10){
+        return('0'+digit);
+    } else {
+        return(digit);
+    }
+}
+
+start.addEventListener("click", function(){
+    let p = document.querySelector("#ipause");
+    p.style.display = 'block';
+
+    let s = document.querySelector("#istart");
+    s.style.display = 'none';
+
+    coun = setInterval(watch, 1000);
+});
+
+pause.addEventListener("click", function(){
+    let p = document.querySelector("#ipause");
+    p.style.display = 'none';
+
+    let s = document.querySelector("#istart");
+    s.style.display = 'block';
+
+    clearInterval(coun);
+});
+
+reset.addEventListener("click", function(){
+    let p = document.querySelector("#ipause");
+    p.style.display = 'none';
+
+    let s = document.querySelector("#istart");
+    s.style.display = 'block';
+
+    clearInterval(coun);
+
+    min = 0;
+    seg = 0;
+
+    document.querySelector("#watch").innerText = "00:00:00";
+});
+
+function watch(){
+    seg++
+
+    if (seg==60){
+        min++;
+        seg = 0;
+    }
+
+    if (min==60){
+        hor++;
+        min = 0;
+    }
+
+    if (hor==24){
+        seg++;
+        hor = 0;
+    }
+
+    document.querySelector("#watch").innerText = twoDigits(hor) + ':' + twoDigits(min) + ':' + twoDigits(seg);
+}
